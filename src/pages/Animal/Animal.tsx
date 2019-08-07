046da3a4733390d57ba230ac97e421ff1fa94d96
jiaoguowei <15135246613@163.com>
2019-08-07 15:21:40 +0800

    '添加大屏动画'

--- src/pages/Animal/Animal.tsx
+++ src/pages/Animal/Animal.tsx
@@ -1,40 +1,118 @@
 import React, { useState, useEffect, useReducer, useRef } from 'react';
 import anime from 'animejs';
 import './Animal.css';
-
+import { bannerData, detailData } from './AnimalConfig'
 
 function Animal(){
+    const [isSelect, setIsSelect] = useState(false)
+    let animalObject: any = {};
+    let innerGroup: any = {};
+    
+    function stop(id: any) {
+        for(var k in animalObject) {
+            animalObject[k].pause()
+        }
+        innerGroup[id].style.display = 'block'
+    }
+    function start(id: any){
+        for(var k in animalObject) {
+            animalObject[k].play()
+        }
+        innerGroup[id].style.display = 'none'
+    }
     useEffect(() => {
-        var path = anime.path('path');
-        console.log('-------', path);
-        // let greenbox = document.querySelector('.green')
+        let tl = anime.timeline({
+            duration: 1000,
+            // complete: function() { tl.restart(); }
+          } as any);
+        let wordGroup: any = document.querySelectorAll('.word');
+        
+        for(var i = 0; i < wordGroup.length; i++) {
+            wordGroup[i].style.left = 80 * i + 'px';
+            // if( i < (wordGroup.length/2 -1) ) {
+            //     wordGroup[i].style.top = 80 * i + 'px';
+            // } else {
+            //     wordGroup[i].style.top = 80 * (wordGroup.length - i) + 'px';
+            // }
+           
+            // tl.add({
+            //     targets: wordGroup[i],
+            //     translateX: 80 * i,
+            //     // translateY: 100,
+            // })
+            // .add({
+            //     targets: wordGroup[i],
+            //     translateY: 50 * i,
+            //     // delay: i * 20
+            // })
+        }
+        // anime({
+        //     targets: '.word',
+        //     translateY: -500,
+        //     duration: 5000,
+        //     loop: true,
+        //     easing: 'linear',
+        //     delay: 100
+        // });
+      
+        // let path = anime.path('path');
+        // bannerData.forEach((item, index) => {
+        //     let bb: any;
+        //     bb = anime({
+        //         targets: `#${item.id}`,
+        //         translateX: {
+        //             value: path('x'),
+        //             duration: 7000,
+        //         },
+        //         translateY: {
+        //             value: path('y'),
+        //             duration: 7000,
+        //         },
+        //         scale: [
+        //             { value: 1.5, duration: 3500 },
+        //             { value: 1, duration: 3500 }
+        //           ],
+        //         loop: true,
+        //         easing: 'linear',
+        //         autoplay: false
+        //       });
+        //       bb.seek(index*1000)
+        //       bb.play()
+        //       animalObject[item.id] = bb
+        // })
 
-        anime({
-        targets: '.green',
-        translateX: path('x'),
-        translateY: path('y'),
-        rotate: path('angle'),
-        easing: 'linear',
-        duration: 5000,
-        loop: true
-        });
-
-        anime({
-        targets: 'path',
-        // opacity: 0,
-        duration: 3000,
-        // loop: true,
-        // direction: 'alternate',
-        easing: 'easeInOutExpo',
-        strokeDashoffset: [anime.setDashoffset, 0],
-        });
     })
     return (
         <div className="animalBox">
-            <svg width="256" height="112" viewBox="0 0 256 112" className="svgBox">
-                <path fill="none" stroke="#FFF" d="M8,56 C8,33.90861 25.90861,16 48,16 C70.09139,16 88,33.90861 88,56 C88,78.09139 105.90861,92 128,92 C150.09139,92 160,72 160,56 C160,40 148,24 128,24 C108,24 96,40 96,56 C96,72 105.90861,92 128,92 C154,93 168,78 168,56 C168,33.90861 185.90861,16 208,16 C230.09139,16 248,33.90861 248,56 C248,78.09139 230.09139,96 208,96 L48,96 C25.90861,96 8,78.09139 8,56 Z"/>
+            <div className="wordBox">
+                {
+                   detailData['one'].map((item, index) => {
+                    return (
+                        <div className="word" key={index}>
+                            {item}
+                        </div>
+                    )
+                   })
+                }
+            </div>
+            <svg  x="0px" y="0px" viewBox="0 0 1920 1080" >
+            <path className="st0" fill="none" stroke="#FFF" d="M959.7,474.1c0,0,142.3,0.5,235.8,16s242.6,44.9,250.1,111.5c0,0,3,70.5-204.6,103.9
+                C1053.7,735.7,960,729.1,960,729.1S779.8,728,691.8,708s-208.7-42.3-218.1-106.4c0,0-16.6-68,240.4-110
+                C714.1,491.6,854.1,471.6,959.7,474.1z"/>
             </svg>
-            <div className="green"></div>
+            {
+                bannerData && bannerData.map((item, index) => {
+                    return (
+                        <div className="circleBanner" id={item.id} onMouseEnter={() => stop(item.id)} onMouseLeave={() => start(item.id)} key={item.id}>
+                            {item.value}
+                            <div className="boxGroup" ref = {(node) => innerGroup[item.id] = node }>
+                                <div className="innerBox1">
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
